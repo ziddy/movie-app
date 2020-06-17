d32a673943169bf5b0ae4daace06389438843f2d
Ziddy <rkdrbehd@gmail.com>
2020-06-17 15:12:24 +0900

    Updates

---
+++ precache-manifest.f20de22196f79e4d73cf7a08725a7790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3d9370bfb6d6f6b1f6a9a7c0fa7fd0",
+    "url": "/ziddy/movie-app/index.html"
+  },
+  {
+    "revision": "861ed0969c6e56f30128",
+    "url": "/ziddy/movie-app/static/css/main.bb46a9f5.chunk.css"
+  },
+  {
+    "revision": "d17a0903073d4ea07fc9",
+    "url": "/ziddy/movie-app/static/js/2.d5845d61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ziddy/movie-app/static/js/2.d5845d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861ed0969c6e56f30128",
+    "url": "/ziddy/movie-app/static/js/main.a12619ca.chunk.js"
+  },
+  {
+    "revision": "ccbd040c855594e113b3",
+    "url": "/ziddy/movie-app/static/js/runtime-main.0472deea.js"
+  }
+]);
